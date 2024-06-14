4d2183c75e496e1de47e347616697d203da757da
Sachintha Sampath <sachinsampath42@gmail.com>
2024-06-14 23:50:40 +0530

    feat: routing

---
+++ src/posts.controller.ts
@@ -0,0 +1,35 @@
+import { Body, Controller, Delete, Get, HttpCode, Param, Post, Put } from '@nestjs/common';
+import PostsService from './posts.service';
+import { CreatePostDto } from './dto/createPost.dto';
+import { UpdatePostDto } from './dto/updatePost.dto';
+
+@Controller('posts')
+export default class PostsController {
+	constructor(private readonly postsService: PostsService) {}
+
+	@Get()
+	getAllPosts() {
+		return this.postsService.getAllPosts();
+	}
+
+	@Get(':id')
+	getPostById(@Param('id') id: string) {
+		return this.postsService.getPostById(Number(id));
+	}
+
+	@Post()
+	async createPost(@Body() post: CreatePostDto) {
+		return this.postsService.createPost(post);
+	}
+
+	@Put(':id')
+	@HttpCode(204)
+	async replacePost(@Param('id') id: string, @Body() post: UpdatePostDto) {
+		return this.postsService.replacePost(Number(id), post);
+	}
+
+	@Delete(':id')
+	async deletePost(@Param('id') id: string) {
+		this.postsService.deletePost(Number(id));
+	}
+}
